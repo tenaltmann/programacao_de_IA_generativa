9b47da94ad69b6b79724804fb4b891697b876407
Ricardo Altmann <96261265+tenaltmann@users.noreply.github.com>
2026-07-22 18:03:37 +0000

    Importando as bibliotecas

---
+++ analise_de_sentimento_de_usuario.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from deep_translator import GoogleTranslator
+
+print("bibliotecas carrecagas")
