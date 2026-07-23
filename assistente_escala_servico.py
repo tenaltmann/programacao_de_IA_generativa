5c55e915a801e5c778e4619eb2f9647224c2d4aa
Ricardo Altmann <96261265+tenaltmann@users.noreply.github.com>
2026-07-23 14:28:38 +0000

    Definindo as variaveis

--- assistente_escala_servico.py
+++ assistente_escala_servico.py
@@ -13,3 +13,17 @@ import spacy
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import re
+
+
+
+## Passo 2 - Importando as bibliotecas
+
+# Baixar o lexico de sentimento de NLTK
+nltk.download('vader_lexicon', quiet=True)
+# Baixar o modelo em portugues do Spacy
+nlp = spacy.load('pt_core_news_sm')
+# Criar o analisador de sentimento
+sia = SentimentIntensityAnalyzer()
+
+print("Tudo foi carregado com sucesso")
+
